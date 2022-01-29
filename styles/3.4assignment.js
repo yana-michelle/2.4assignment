ab287c2a0e8415dbfc23f538b2186bcafdaf4293
yana-michelle <adrianna.aleman22@outlook.com>
2022-01-28 19:45:57 -0600

    const and block

--- styles/3.4assignment.js
+++ styles/3.4assignment.js
@@ -2,6 +2,13 @@ var life = 100; //global
 
 life = life - 36;
 
-let j = 20; //block
+{
+    let j = 20; //block
+}
 
-console.log(life);
+var name = 'Adrianna'; //global string
+
+const creator = { name: 'Adrianna', lastName: 'Aleman', age: 21};
+
+console.log(creator);
+console.log(name)
